184bafd4b918e7f023c2484077bb683d64f2cd9a
Joaquin <Joaquinlopez21@outlook.es>
2025-03-28 18:30:14 -0300

    add main logic

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,17 +1,28 @@
 // components/ItemCard.tsx
 import { GameItem } from "../types/GameItem";
 
-const ItemCard = ({ item }: { item: GameItem }) => {
+interface ItemCardProps {
+    item: GameItem,
+    showValue: boolean
+}
+
+
+const ItemCard = ({ item, showValue }: ItemCardProps) => {
     return (
         <div className="flex flex-col items-center p-4 border rounded-lg shadow-lg">
             <img src={item.image} className="w-60 h-40 object-cover mb-4" />
             <h2 className="text-lg">{item.name}</h2>
 
-            {/* Verificamos si es un auto o una película */}
-            {item.type === "car" ? (
-                <p className="text-md text-yellow-300">Price: ${item.price}</p>
+            {/* Check if 'showValue' is true and then display either the price or box office depending on item type (car or movie) */}
+            {showValue ? (
+                item.type === "car" ? (
+                    <p className="text-md text-yellow-500 ">Price: ${item.price}</p>
+                ) : (
+                    <p className="text-md text-blue-400">Box Office: ${item.boxOffice}</p>
+                )
             ) : (
-                <p className="text-md text-blue-400">Box Office: ${item.boxOffice}</p>
+                // Render an empty paragraph when 'showValue' is false to hide the price/box office
+                <p className="text-md text-gray-400"></p>
             )}
         </div>
     );
--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { GameItem } from "../types/GameItem";
 import { mockItems } from "../utils/mockItems";
@@ -8,16 +9,23 @@ const getGameItems = (gameType: string | null): GameItem[] => {
     return mockItems.filter((item) => item.type === gameType);
 };
 
+const getItemValue = (item: GameItem): number => {
+    return item.type === "car" ? item.price : item.boxOffice
+}
+
 const Game = () => {
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
     const gameType = searchParams.get("type");
 
     // Get filtered items based on the selected game type
-    const items = getGameItems(gameType);
+    const filteredItems = getGameItems(gameType);
+
+    const [remainingItems, setRemainingItems] = useState<GameItem[]>(filteredItems.slice(2))
+    const [currentItems, setCurrentItems] = useState<GameItem[]>(filteredItems.slice(0, 2))
 
     // Early validation: If gameType is invalid or there aren't at least two items, show an error message
-    if (!gameType || items.length < 2) {
+    if (!gameType || filteredItems.length < 2) {
         return (
             <h1 className="text-center mt-10 text-red-500">
                 There's not enough elements to play
@@ -26,7 +34,42 @@ const Game = () => {
     }
 
     // Select the first two items to be compared
-    const [item1, item2] = items;
+    const [item1, item2] = currentItems
+
+    const getRandomItem = (items: GameItem[]): GameItem | null => {
+        if (items.length === 0) return null;
+        const randomIndex = Math.floor(Math.random() * items.length);
+        return items[randomIndex]
+    }
+
+    const handleGuess = (choice: "higher" | "lower") => {
+        const value1 = getItemValue(item1)
+        const value2 = getItemValue(item2)
+
+        const isCorrect =
+            (choice === "higher" && value2 >= value1) ||
+            (choice === "lower" && value2 <= value1)
+
+        if (isCorrect) {
+            // Choose a random item from remainingItems
+            const randomItem = getRandomItem(remainingItems.filter(item => item !== item2));
+
+            // Handle Exception if no more items are available to show
+            if (!randomItem) {
+                console.log("No more items available")
+                return;
+            }
+
+            // Update status with new pair of items
+            setCurrentItems([item2, randomItem])
+
+            // Delete item1 from remainingItems
+            setRemainingItems((prev) => prev.filter(item => item !== item1))
+
+        } else {
+            console.log("You failed!")
+        }
+    }
 
     return (
         <div className="flex flex-col items-center">
@@ -34,8 +77,12 @@ const Game = () => {
                 {gameType === "car" ? "Which car is more expensive?" : "Which movie made more money?"}
             </h1>
             <div className="flex space-x-8">
-                <ItemCard item={item1} />
-                <ItemCard item={item2} />
+                <ItemCard item={item1} showValue={true} />
+                <ItemCard item={item2} showValue={false} />
+            </div>
+            <div className="flex flex-col gap-3 m-5">
+                <button onClick={() => handleGuess("higher")} className="border-2 border-black p-2 cursor-pointer">Higher</button>
+                <button onClick={() => handleGuess("lower")} className="border-2 border-black p-2 cursor-pointer">Lower</button>
             </div>
         </div>
     );
